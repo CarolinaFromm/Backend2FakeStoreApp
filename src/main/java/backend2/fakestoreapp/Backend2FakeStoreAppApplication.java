14e1385559e0ac0d0bca65d2557607186f2f9b13
Fatima <fatimazhar@hotmail.com>
2025-08-20 15:23:25 +0200

    Initial commit

---
+++ src/main/java/backend2/fakestoreapp/Backend2FakeStoreAppApplication.java
@@ -0,0 +1,13 @@
+package backend2.fakestoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2FakeStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend2FakeStoreAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend2/fakestoreapp/Backend2FakeStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package backend2.fakestoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend2FakeStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
