52ba5640719f8cef791b9c8ab4000fb7e7ce3a41
Fatima <fatimazhar@hotmail.com>
2025-08-20 16:04:54 +0200

    Added role to the migration script (arent implemented yet, waiting to implement all together) created models customer and role based on the database entities.

--- src/main/resources/db/migration/V1__create_table_customer.sql
+++ src/main/resources/db/migration/V1__create_table_customer.sql
@@ -1,10 +1,16 @@
 -- Customer table
 CREATE TABLE customer (
-                        id SERIAL PRIMARY KEY,
-                        name VARCHAR(255) NOT NULL,
-                        address VARCHAR(255),
-                        email VARCHAR(255),
-                        phone VARCHAR(50),
-                        password VARCHAR(64),
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(50),
+    password VARCHAR(64),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Role table
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
 );
