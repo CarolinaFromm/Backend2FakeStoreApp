552374ce4bf85b24bceed9a6a31e9df6651bdc4a
Fatima <fatimazhar@hotmail.com>
2025-08-20 15:55:23 +0200

    created customer table flyway migration script. All databasse changes from now on will be changed/addedby flyway migration scripts.

---
+++ src/main/resources/db/migration/V1__create_table_customer.sql
@@ -0,0 +1,10 @@
+-- Customer table
+CREATE TABLE customer (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(255) NOT NULL,
+                        address VARCHAR(255),
+                        email VARCHAR(255),
+                        phone VARCHAR(50),
+                        password VARCHAR(64),
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
