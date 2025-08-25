8a2d2ce2c0fc82df032f290f8a2e6a941abb7cfe
Fatima <fatimazhar@hotmail.com>
2025-08-25 15:27:58 +0200

    fixed customer repository

--- src/main/java/backend2/fakestoreapp/repository/CustomerRespository.java
+++ src/main/java/backend2/fakestoreapp/repository/CustomerRespository.java
@@ -1,6 +1,7 @@
 package backend2.fakestoreapp.repository;
 
+import backend2.fakestoreapp.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRespository extends JpaRepository {
+public interface CustomerRespository extends JpaRepository <Customer, Long> {
 }
