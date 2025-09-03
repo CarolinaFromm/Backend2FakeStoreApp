2e03597e29cda5b2589fad60be09fa4de60a497e
Ardian Rama <ardian.rama@hotmail.se>
2025-09-03 15:27:12 +0200

    Finished ProductServiceImplTest

--- src/main/java/backend2/fakestoreapp/DTO/ProductDTO.java
+++ src/main/java/backend2/fakestoreapp/DTO/ProductDTO.java
@@ -1,13 +1,13 @@
 package backend2.fakestoreapp.DTO;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class ProductDTO {
 
